31e8f853329d1b5a26a10c66aba4cf22d392b70c
isleap9 <pierluigisassanelli98@gmail.com>
2026-04-05 13:30:35 +0200

    add wlogout, pywal theming, cursor config

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -64,5 +64,50 @@ swaync-client -R
 
 pkill swaync; sleep 0.5 && swaync &
 
+# Update wlogout colors
+cat > ~/.config/wlogout/style.css << EOF
+* {
+    background-image: none;
+    box-shadow: none;
+}
+window {
+    background-color: rgba(0,0,0,0.5);
+}
+button {
+    border-radius: 12px;
+    border-color: ${color0};
+    text-decoration-color: ${foreground};
+    color: ${foreground};
+    background-color: ${color0};
+    border-style: solid;
+    border-width: 1px;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 25%;
+}
+button:focus, button:active, button:hover {
+    background-color: ${color2};
+    outline-style: none;
+}
+#lock {
+    background-image: image(url("/usr/share/wlogout/icons/lock.png"), url("/usr/local/share/wlogout/icons/lock.png"));
+}
+#logout {
+    background-image: image(url("/usr/share/wlogout/icons/logout.png"), url("/usr/local/share/wlogout/icons/logout.png"));
+}
+#suspend {
+    background-image: image(url("/usr/share/wlogout/icons/suspend.png"), url("/usr/local/share/wlogout/icons/suspend.png"));
+}
+#hibernate {
+    background-image: image(url("/usr/share/wlogout/icons/hibernate.png"), url("/usr/local/share/wlogout/icons/hibernate.png"));
+}
+#shutdown {
+    background-image: image(url("/usr/share/wlogout/icons/shutdown.png"), url("/usr/local/share/wlogout/icons/shutdown.png"));
+}
+#reboot {
+    background-image: image(url("/usr/share/wlogout/icons/reboot.png"), url("/usr/local/share/wlogout/icons/reboot.png"));
+}
+EOF
+
 sleep 0.3
 pkill waybar && waybar &
